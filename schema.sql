a285c3c6fbc80265adfa2a0d3afdac416229be81
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2023-01-25 16:09:46 +0100

    Finish: Create Table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,12 @@
 /* Database schema to keep the structure of entire database. */
+-- CREATE TABLE animals (
+--     name varchar(100)
+-- );
 
 CREATE TABLE animals (
-    name varchar(100)
-);
+    id INT, 
+    name CHAR, 
+    date_of_birth DATE, 
+    escape_attempts INT, 
+    neutered BIT, 
+    weight_kg REAL)
