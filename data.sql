f8d015a2206bd9c56b181e515e970f247f13f2a5
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2023-01-26 08:41:29 +0100

    Fix: Clear everything

--- data.sql
+++
@@ -1,6 +0,0 @@
-/* Populate database with sample data. */
-
-INSERT INTO animals VALUES (1, 'Agumon', '03-02-2020', 0, TRUE, 10.23)
-INSERT INTO animals VALUES (2, 'Gabumon', '15-11-2018', 2, TRUE, 8)
-INSERT INTO animals VALUES (3, 'Pikachu', '07-01-2021', 1, FALSE, 15.04)
-INSERT INTO animals VALUES (4, 'Devimon', '12-06-2017', 5, TRUE, 11)
