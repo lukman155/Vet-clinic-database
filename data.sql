931e95b759617847bbe330b55ad655f4ac6d1fe9
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2023-01-28 18:20:59 +0100

    Finish: Part 2

--- data.sql
+++ data.sql
@@ -5,3 +5,12 @@ INSERT INTO animals VALUES (2, 'Gabumon', '15-11-2018', 2, TRUE, 8)
 INSERT INTO animals VALUES (3, 'Pikachu', '07-01-2021', 1, FALSE, 15.04)
 INSERT INTO animals VALUES (4, 'Devimon', '12-06-2017', 5, TRUE, 11)
 
+INSERT INTO animals (id,name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES        
+        (5,'Charmander', '2022-02-08', 0, 'FALSE', -11),
+        (6,'Plantmon', '2021-11-15', 2, 'TRUE', -5.7), 
+        (7,'Squirtle', '1993-04-02', 3, 'FALSE', -12.13), 
+        (8,'Angemon', '2005-06-12', 1, 'TRUE', -45),
+        (9,'Boarmon', '2005-06-07', 7, 'TRUE', 20.4), 
+        (10,'Blossom', '1998-08-13', 3, 'TRUE', 17), 
+        (11,'Ditto', '2022-05-14', 4, 'TRUE', 22)
+;
